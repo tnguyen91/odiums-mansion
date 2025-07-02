a5cc917ac7046948613a22b4b899c241baa87830
tnguyen91 <nmt090102@gmail.com>
2025-07-02 01:18:55 -0700

    add hiding tiles at the beginning

--- src/GameWindow.cpp
+++ src/GameWindow.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "Dungeon.h"
 
-GameWindow::GameWindow() : window(sf::VideoMode(640, 640), "Odium's Mansion - SFML") {}
+GameWindow::GameWindow() : window(sf::VideoMode(640, 640), "Odium's Mansion") {}
 
 void GameWindow::run(std::array<std::array<Tile, DUNGEON_SIZE>, DUNGEON_SIZE>& dungeon) {
     Player player(dungeon);
@@ -23,20 +23,26 @@ void GameWindow::run(std::array<std::array<Tile, DUNGEON_SIZE>, DUNGEON_SIZE>& d
                 }
             } 
         }
-        drawGrid(dungeon, player.getCoord());
-        auto [r, c] = player.getCoord();
-        Tile current = dungeon[r][c];
+        std::pair<int, int> pos = player.getCoord();
+        dungeon[pos.first][pos.second].isRevealed = true;
+        drawGrid(dungeon, pos);
+        Tile current = dungeon[pos.first][pos.second];
 
         if (current.hasPit) {
             std::cout << "You fell into a pit! Game Over.\n";
             sf::sleep(sf::seconds(2));
             window.close();
-        } else if (current.hasEnemy) {
+        } else if (current.hasEvil) {
             std::cout << "You encountered evil! Game Over.\n";
             sf::sleep(sf::seconds(2));
             window.close();
         } else if (current.hasGold) {
-            std::cout << "You found the gold! You win!\n";
+            std::cout << "You found the gold! Now return to (0,0) to escape.\n";
+            current.hasGold = false;
+            player.setCarryingGold(true);
+            dungeon[pos.first][pos.second] = current;
+        } else if (player.isCarryingGold() && player.getCoord() == std::make_pair(0, 0)) {
+            std::cout << "You escaped with the gold! You win!\n";
             sf::sleep(sf::seconds(2));
             window.close();
         }
@@ -58,24 +64,18 @@ void GameWindow::drawGrid(const std::array<std::array<Tile, DUNGEON_SIZE>, DUNGE
 
             const Tile& t = dungeon[i][j];
 
-            if (playerPos.first == i && playerPos.second == j) {
-                cell.setFillColor(sf::Color::Green);
-            } else if (t.hasEnemy) {
-                cell.setFillColor(sf::Color::Red);
-            } else if (t.hasGold) {
-                cell.setFillColor(sf::Color::Yellow);
-            } else if (t.hasPit) {
-                cell.setFillColor(sf::Color(139, 69, 19));  // Brown
-            } else if (t.hasStench && t.hasBreeze) {
-                cell.setFillColor(sf::Color(186, 85, 211)); // Violet
-            } else if (t.hasStench) {
-                cell.setFillColor(sf::Color::Magenta);
-            } else if (t.hasBreeze) {
-                cell.setFillColor(sf::Color::Cyan);
+            if (t.isRevealed) {
+                if (i == playerPos.first && j == playerPos.second) 
+                    cell.setFillColor(sf::Color::Green); // Player position
+                else if (t.hasPit) cell.setFillColor(sf::Color::Black);
+                else if (t.hasEvil) cell.setFillColor(sf::Color::Red);
+                else if (t.hasGold) cell.setFillColor(sf::Color::Yellow);
+                else if (t.hasStench) cell.setFillColor(sf::Color(128, 0, 128)); // Purple for stench
+                else if (t.hasBreeze) cell.setFillColor(sf::Color::Cyan);
+                else cell.setFillColor(sf::Color::White);
             } else {
-                cell.setFillColor(sf::Color(80, 80, 80)); // Empty tile
+                cell.setFillColor(sf::Color(169, 169, 169)); // Dark gray for unrevealed
             }
-
             window.draw(cell);
         }
     }
